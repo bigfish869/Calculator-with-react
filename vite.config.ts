d7d458dc7f9e04f9da3835094ba958452b52e5a6
bigfish869 <azolami2009@gmail.com>
2026-08-17 23:31:28 +0100

    Fix Vite config to include React plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: '/Calculator-with-react/',  // <-- ADD THIS LINE
+  plugins: [react()],
+  base: '/Calculator-with-react/',
 })
