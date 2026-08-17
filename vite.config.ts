45ec2b91f3550b4c6319dff24133726466ffb287
bigfish869 <azolami2009@gmail.com>
2026-08-17 22:39:57 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Calculator-with-react/"  // <-- ADD THIS LINE
+  base: '/Calculator-with-react/',  // <-- ADD THIS LINE
 })
